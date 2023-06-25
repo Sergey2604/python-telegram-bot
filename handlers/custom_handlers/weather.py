ddb8584f5d4af7f1bd6520f10c8b088698d2e211
Sergey Lebedev <sergeylebedev87@gmail.com>
2023-06-25 12:31:40 +0300

    current weather in any city by russian language

--- handlers/custom_handlers/weather.py
+++ handlers/custom_handlers/weather.py
@@ -1,5 +1,6 @@
 from telebot.types import Message
 
+from database.common.models import db
 from loader import bot
 from states.user_states import UserInfoState
 from work_without_telegram import to_work
@@ -13,19 +14,16 @@ def weather(message: Message) -> None:
     bot.send_message(message.chat.id, "Введите город, в котором Вы хотите узнать погоду")
 
 
-city = None
-
-
 @bot.message_handler(state=UserInfoState.city)
-def get_city(message: Message) -> None:
+def get_city_and_temp(message: Message) -> None:
     if message.text.isalpha():
         bot.send_message(message.chat.id, 'Сейчас будет исполнено')
         bot.set_state(message.from_user.id, UserInfoState.form_weather, message.chat.id)
         to_work(message.from_user.id, message.text)
+        temp = db.execute_sql("SELECT `temp` FROM history WHERE `user_id` = ? AND place=? ORDER BY id DESC LIMIT 1", (
+            message.from_user.id, message.text
+        ))
+        for i in temp:
+            bot.send_message(message.chat.id, 'Сейчас в городе {0} {1} градусов'.format(message.text, *i))
     else:
         bot.send_message(message.from_user.id, 'В названии города должны быть только буквы')
-
-
-@bot.message_handler(state=UserInfoState.form_weather)
-def form_weather(message: Message) -> None:
-    bot.send_message(message.chat.id, 'Сейчас в городе {0} {1} градусов'.format(city, temp))
